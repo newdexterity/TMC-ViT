f77d46ce71bd0cb50b7a504c744fc503f978bc65
Felipe Padula Sanches <fpadula92@gmail.com>
2022-04-17 18:00:44 -0300

    Code cleanup

--- model.py
+++ model.py
@@ -2,54 +2,7 @@ import numpy as np
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Flatten, Dense, Activation, Dropout, Conv2D, MaxPooling2D, Softmax, BatchNormalization
-from tensorflow.keras.optimizers import Adam
-
-
-
-# Load data
-X_test = np.load('X_test.npy')
-X_train = np.load('X_train.npy')
-y_test = np.load('Y_test.npy')
-y_train = np.load('Y_train.npy')
-
-## Reshape in order to X_train and test be used as input to the CNNN
-X_train = X_train.reshape(-1, 16, 40, 1)
-X_test = X_test.reshape(-1, 16, 40, 1)
-
-
-num_classes = 18
-input_shape = (16, 40, 1)
-image_size1 = 16
-image_size2= 20
-patch_size = 4
-num_patches = (image_size1 // patch_size) * (image_size2 // patch_size)
-projection_dim = 64
-num_heads = 4
-transformer_units = [
-    projection_dim * 2,
-    projection_dim,
-]  # Size of the transformer layers
-transformer_layers = 8
-mlp_head_units = [2048, 1024]  # Size of the dense layers of the final classifier
-
-token_emb = keras.Sequential(
-    [
-        Conv2D(16, (8, 8), activation="relu", padding="same", input_shape=[X_train.shape[1], X_train.shape[2], 1]),
-        BatchNormalization(),
-        MaxPooling2D((1, 2)),
-        Dropout(0.3),
-        Conv2D(32, (4, 4), activation="relu", padding="same"),
-        BatchNormalization(),
-        Dropout(0.3),
-        Conv2D(64, (2, 2), activation="relu", padding="same"),
-        BatchNormalization(),
-    ],
-    name="token_emb",
-)
-
-
+from tensorflow.keras.layers import Dropout, Conv2D, MaxPooling2D, BatchNormalization
 
 def mlp(x, hidden_units, dropout_rate):
     for units in hidden_units:
@@ -89,7 +42,9 @@ class PatchEncoder(layers.Layer):
         encoded = self.projection(patch) + self.position_embedding(positions)
         return encoded
         
-def create_tmc_vit_classifier():
+def create_tmc_vit_classifier(input_shape, token_emb, patch_size, num_patches,
+        projection_dim, transformer_layers, num_heads, transformer_units,
+        mlp_head_units):
     inputs = layers.Input(shape=input_shape)
     # Token embedding.
     tokenemb = token_emb(inputs)
@@ -120,7 +75,8 @@ def create_tmc_vit_classifier():
     representation = layers.Flatten()(representation)
     representation = layers.Dropout(0.5)(representation)
     # Add MLP.
-    features = mlp(representation, hidden_units=mlp_head_units, dropout_rate=0.5)
+    features = mlp(representation, hidden_units=mlp_head_units, 
+            dropout_rate=0.5)
     # Classify outputs.
     logits = layers.Dense(18, activation="softmax")(features)
     # Create the Keras model.
@@ -128,11 +84,62 @@ def create_tmc_vit_classifier():
     return model
 
 
-model = create_tmc_vit_classifier()
+def main():
+    # Load data
+    X_test = np.load('./data/X_test_200_3.npy')
+    X_train = np.load('./data/X_train_200_3.npy')
+    y_test = np.load('./data/Y_test_200_3.npy')
+    y_train = np.load('./data/Y_train_200_3.npy')
 
-model.compile(loss='sparse_categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+    ## Reshape in order to X_train and test be used as input to the CNNN
+    X_train = X_train.reshape(-1, 16, 40, 1)
+    X_test = X_test.reshape(-1, 16, 40, 1)
 
-callback = tf.keras.callbacks.EarlyStopping(monitor='val_accuracy', min_delta=0, patience=70, mode='max', restore_best_weights=True)
+    num_classes = 18
+    input_shape = (16, 40, 1)
+    image_size1 = 16
+    image_size2= 20
+    patch_size = 4
+    num_patches = (image_size1 // patch_size) * (image_size2 // patch_size)
+    projection_dim = 64
+    num_heads = 4
+    transformer_units = [
+        projection_dim * 2,
+        projection_dim,
+    ]  # Size of the transformer layers
+    transformer_layers = 8
+    mlp_head_units = [2048, 1024]  # Size of the dense layers of the final classifier
 
-history = model.fit(X_train, y_train, batch_size=128, epochs=500, verbose = 0, validation_data=(X_test, y_test),callbacks=[callback])
+    token_emb = keras.Sequential(
+        [
+            Conv2D(16, (8, 8), activation="relu", padding="same", 
+                    input_shape=[X_train.shape[1], X_train.shape[2], 1]),
+            BatchNormalization(),
+            MaxPooling2D((1, 2)),
+            Dropout(0.3),
+            Conv2D(32, (4, 4), activation="relu", padding="same"),
+            BatchNormalization(),
+            Dropout(0.3),
+            Conv2D(64, (2, 2), activation="relu", padding="same"),
+            BatchNormalization(),
+        ],
+        name="token_emb",
+    )
 
+    model = create_tmc_vit_classifier(input_shape, token_emb, patch_size, 
+            num_patches, projection_dim, transformer_layers, num_heads, 
+            transformer_units, mlp_head_units)
+
+    model.compile(loss='sparse_categorical_crossentropy', optimizer='adam', 
+            metrics=['accuracy'])
+
+    callback = tf.keras.callbacks.EarlyStopping(monitor='val_accuracy', 
+            min_delta=0, patience=70, mode='max', restore_best_weights=True)
+
+    print("Starting training...")
+    history = model.fit(X_train, y_train, batch_size=128, epochs=500, 
+            verbose = 1, validation_data=(X_test, y_test),callbacks=[callback])
+    print("Training finished!")
+
+if __name__ == '__main__':
+    main()    
